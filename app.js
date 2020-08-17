63bcc6d843a84ea58b810f91fb8461156da60d7a
Maganth Seetharaman <maganth.seetharaman@freshworks.com>
2020-08-17 09:51:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.post('/', (req, res) => {
+    console.log(getDate());
+    res.sendStatus(200);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+function getDate() {
+    var currentdate = new Date();
+    var datetime = "Last Sync: "  + currentdate.getTime() + " "
+        + currentdate.getDate() + "/"
+        + (currentdate.getMonth()+1)  + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
+    return datetime
+}
