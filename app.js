ad0682a4b2b6671a541c0c4e842cbd53ad7b86c3
Maganth Seetharaman <maganth.seetharaman@freshworks.com>
2020-09-17 11:05:38 +0530

    Print request body

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
 app.post('/', (req, res) => {
-    console.log(getDate());
-    res.sendStatus(200);
+    console.log(getDate())
+    console.log(req.body)
+    res.sendStatus(200)
 })
 
 app.listen(port, () => {
